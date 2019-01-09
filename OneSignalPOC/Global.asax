484df8393631b23d76ed326888ce1a3fa3f93043
Sajjad Gul <sajjadarifgul@gmail.com>
2019-01-09 15:45:40 +0500

    Add project files.

---
+++ OneSignalPOC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneSignalPOC.MvcApplication" Language="C#" %>
---
+++ OneSignalPOC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneSignalPOC.Startup))]
+namespace OneSignalPOC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
